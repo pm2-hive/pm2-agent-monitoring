3d74d5f3a5eedba7d4558ca254d0567582c7c97a
Unitech <strzelewicz.alexandre@gmail.com>
2017-01-11 21:29:00 +0100

    1.3.0

--- probe.js
+++ probe.js
@@ -13,32 +13,49 @@ var conf = pmx.initModule({
   module_type      : 'database',
   pid              : pmx.getPID(path.join(process.env.HOME, '.pm2', 'agent.pid')),
   bg_color  : '#333333',
-  logo : 'https://keymetrics.io/assets/images/pm2.20d3ef.png?v=0b71a506ce'
+  widget : {
+    logo : 'https://keymetrics.io/assets/images/pm2.20d3ef.png?v=0b71a506ce',
+    block : {
+      main_probes : ['events/min']
+    }
+  }
 });
 
 var probe = pmx.probe();
 
 var pm2_procs = 0;
 
+var metric = probe.metric({
+  name  : 'Processes',
+  value : function() {
+    return pm2_procs;
+  }
+});
+
+var event_metric = probe.meter({
+  name : 'events/min'
+});
+
 pm2.connect(function() {
 
+  pm2.launchBus(function(err, bus) {
+    bus.on('*', function(event, data) {
+      if (event.indexOf('log:') > -1)
+        event_metric.mark();
+    });
+  });
   setInterval(function() {
     pm2.list(function(err, procs) {
       pm2_procs = procs.length;
     });
   }, 2000);
 
-  var metric = probe.metric({
-    name  : 'Processes',
-    value : function() {
-      return pm2_procs;
-    }
-  });
+
 });
 
 process.on('SIGINT', function() {
   pm2.disconnect();
   setTimeout(function() {
     process.exit(0);
-  }, 1000);
+  }, 100);
 });
